d35026e2424b471cf32f8fe342bfd8ab6c17a91e
Tishana <tishana.trainor@gmail.com>
2023-07-27 13:49:06 -0400

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -1,5 +1,27 @@
 // Easy Going
 
-for(i=1;i<=20;i++){
+for(i = 1;i <= 20;i++){
     console.log(i)
+}
+
+// Get Even
+
+for(i = 0; i <= 200; i++){
+    if(i % 2 == 0){
+        console.log(i)
+    }
+}
+ 
+// Fizz Buzz
+
+for(i = 1; i <= 100; i++){
+    if(i % 5 == 0 && i % 3 == 0 ){
+        console.log("FizzBuzz")
+    } else if(i%5 == 0){
+        console.log("Buzz")
+    } else if(i % 3 == 0){
+        console.log("Fizz")
+    }else{
+        console.log(i)
+    }
 }
